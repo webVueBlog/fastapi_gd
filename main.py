b4f0dc411f54fb74b43d9e6968e90e9a4ca452db
webVueBlog <2979986590@qq.com>
2025-05-11 14:37:16 +0800

    python-fastapi_gd

---
+++ main.py
@@ -0,0 +1,12 @@
+# main.py
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/")
+def read_root():
+    return {"message": "Hello, FastAPI from Windows!"}
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: str = None):
+    return {"item_id": item_id, "q": q}
